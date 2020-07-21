4dcd759c54e23e43bde8c2bfd68794080422c8fe
santiagopemo <santiagopemo@gmail.com>
2020-07-21 23:38:19 +0000

    added task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,7 +8,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t r, l;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 		return (0);
 	l = binary_tree_height(tree->left);
 	r = binary_tree_height(tree->right);
