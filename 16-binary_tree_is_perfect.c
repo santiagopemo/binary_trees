a27c91cadb3873688f30cdbe8b3495dc59e03e9c
santiagopemo <santiagopemo@gmail.com>
2020-07-22 15:16:09 +0000

    added task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -23,13 +23,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t r, l;
-
 	if (tree == NULL)
 		return (0);
-	l = binary_tree_height(tree->left);
-	r = binary_tree_height(tree->right);
-	if (l == r)
+	if (tree->right == NULL && tree->left == NULL)
 		return (1);
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+	{
+		if (binary_tree_is_perfect(tree->left)
+				&& binary_tree_is_perfect(tree->right))
+			return (1);
+	}
 	return (0);
 }
